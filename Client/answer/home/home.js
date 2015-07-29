ec8fc6f54e90a63e5ddae915a9797c5a3775a263
Nathan <nathansherburn@gmail.com>
2015-07-29 13:17:48 +1000

    Add poll results page

--- Client/answer/home/home.js
+++ Client/answer/home/home.js
@@ -10,49 +10,52 @@ app.controller('AnswerHomeController', function($scope, $routeParams, $location,
 	$scope.subscribed = true;
 	$scope.activePolls = [];
 	$scope.selectedPoll = null;
+	$scope.pollIndex = 0;
 
 	// Simulate retrieving active polls
-	$timeout(activePollsRecieved, 1500);
+	$timeout(activePollsRecieved, 1000);
 
 	// Simulate recieving a new poll
-	$timeout(newPoll, 6000);
+	$timeout(newPoll, 1000);
 
 	function activePollsRecieved() {
 		$scope.activePolls = [
-			// {
-			// 	collection: "ENG1001",
-			// 	question: "What is this subject? An ideal inverting Op Amp?",
-			// 	answers: [
-			// 		{ label: 'The gain, G, would be 0.3333', value: 1 },
-			// 		{ label: 'The gain, G, would be 3', value: 2 },
-			// 		{ label: 'The gain, G, would be 3*10^4', value: 3 },
-			// 		{ label: 'The gain, G, would be -3', value: 4 }
-			// 	]
-			// },
-			// {
-			// 	collection: "ENG1002",
-			// 	question: "Assuming an ideal inverting Op Amp, what would the gain be, if R1 was 300k ohms and R2 was 100k?",
-			// 	answers: [
-			// 		{ label: 'The gain, G, would be 0.3333', value: 1 },
-			// 		{ label: 'The gain, G, would be 3', value: 2 },
-			// 		{ label: 'The gain, G, would be 3*10^4', value: 3 },
-			// 		{ label: 'The gain, G, would be -3', value: 4 }
-			// 	]
-			// },
+			{
+				collection: "ENG1001",
+				question: "What is this subject? An ideal inverting Op Amp?",
+				answers: [
+					{ label: 'The gain, G, would be 0.3333' },
+					{ label: 'The gain, G, would be 3' },
+					{ label: 'The gain, G, would be 3*10^4' },
+					{ label: 'The gain, G, would be -3' }
+				]
+			},
+			{
+				collection: "ENG1002",
+				question: "Assuming an ideal inverting Op Amp, what would the gain be, if R1 was 300k ohms and R2 was 100k?",
+				answers: [
+					{ label: 'The gain, G, would be 0.3333' },
+					{ label: 'The gain, G, would be 3' },
+					{ label: 'The gain, G, would be 3*10^4' },
+					{ label: 'The gain, G, would be -3' }
+				]
+			},
 			{
 				collection: "ENG1003",
 				question: "How many different data types are there in JavaScript?",
 				answers: [
-					{ label: 'The gain, G, would be 0.3333', value: 1 },
-					{ label: 'The gain, G, would be 3', value: 2 },
-					{ label: 'The gain, G, would be 3*10^4', value: 3 },
-					{ label: 'The gain, G, would be -3', value: 4 }
+					{ label: 'The gain, G, would be 0.3333' },
+					{ label: 'The gain, G, would be 3' },
+					{ label: 'The gain, G, would be 3*10^4' },
+					{ label: 'The gain, G, would be -3' }
 				]
 			}
 		];
 
-		// If there's only one poll, set it as the activePoll
-		if ($scope.activePolls.length === 1) $scope.selectedPoll = $scope.activePolls[0];
+		// Set first poll as active poll
+		if ($scope.activePolls.length > 0) {
+			$scope.selectedPoll = $scope.activePolls[0];
+		}
 	}
 
 	function newPoll () {
@@ -66,16 +69,33 @@ app.controller('AnswerHomeController', function($scope, $routeParams, $location,
 					{ label: 'The gain, G, would be -3', value: 4 }
 				]
 			})
+
+		if ($scope.activePolls.length === 1) {
+			$scope.selectedPoll = $scope.activePolls[0];
+		}
 	}
 
-	$scope.selectPoll = function (poll) {
-		console.log(poll)
-		$scope.selectedPoll = poll;
+	// $scope.selectPoll = function (poll) {
+	// 	console.log(poll)
+	// 	$scope.selectedPoll = poll;
+	// }
+
+	$scope.nextPoll = function (event) {
+		if ($scope.pollIndex > $scope.activePolls.length - 2) {
+			$scope.pollIndex = 0;
+		} else {
+			$scope.pollIndex++;
+		}
+		$scope.selectedPoll = $scope.activePolls[$scope.pollIndex]
 	}
 
-	$scope.submit = function () {
-		console.log("Sending answers...")
-		$scope.selectedPoll = null;
+	// $scope.submit = function () {
+	// 	console.log("Sending answers...")
+	// 	$scope.selectedPoll = null;
+	// }
+
+	$scope.submitAnswer = function () {
+		console.log("sending answer");
 	}
 
 	$scope.alert = '';
--- Client/results/viewResults.js
+++ Client/results/viewResults.js
@@ -1,3 +1,75 @@
-app.controller('PollResultsController', function($scope, $routeParams, $location, $mdDialog) {
+app.controller('PollResultsController', function($scope, $routeParams, $location, $mdDialog, $interval) {
+	// TODO: Request melts code and results
+
+	$scope.pollCode = "QF177" 
+	$scope.highestVotes = 0;
+
+	$scope.results = [
+		{ option: "Answer 1", votes: 13 },
+		{ option: "Answer 2", votes: 5 },
+		{ option: "Answer 3", votes: 15 },
+		{ option: "Answer 4", votes: 15 },
+		{ option: "Answer 4", votes: 15 },
+		{ option: "Answer 4", votes: 15 },
+		{ option: "Answer 4", votes: 15 },
+		{ option: "Answer 4", votes: 15 },
+		{ option: "Answer 4", votes: 15 },
+		{ option: "Answer 4", votes: 15 },
+	] 
+
+	// Simulate new votes
+	$interval(newVotes, 500);
+
+	function newVotes() {
+		for (var i = 0; i < $scope.results.length; i++) {
+			$scope.results[i].votes += Math.floor(Math.random()*5) - 2;
+		}
+
+		for (var i = 0; i < $scope.results.length; i++) {
+			if ($scope.results[i].votes > $scope.highestVotes) {
+				$scope.highestVotes = $scope.results[i].votes;
+			}
+		}
+	}
+
+
+	// Color Generator
+	// Adapted from: http://stackoverflow.com/questions/17242144/javascript-convert-hsb-hsv-color-to-rgb-accurately
+	function HSVtoRGB(h, s, v) {
+	    var r, g, b, i, f, p, q, t;
+
+	    h = h/360
+	    i = Math.floor(h * 6);
+	    f = h * 6 - i;
+	    p = v * (1 - s);
+	    q = v * (1 - f * s);
+	    t = v * (1 - (1 - f) * s);
+	    switch (i % 6) {
+	        case 0: r = v, g = t, b = p; break;
+	        case 1: r = q, g = v, b = p; break;
+	        case 2: r = p, g = v, b = t; break;
+	        case 3: r = p, g = q, b = v; break;
+	        case 4: r = t, g = p, b = v; break;
+	        case 5: r = v, g = p, b = q; break;
+	    }
+	    console.log(r,g,b)
+
+	    var red = Math.floor(r * 255).toString(16);
+	    var grn = Math.floor(g * 255).toString(16);
+	    var blu = Math.floor(b * 255).toString(16);
+
+	    return {
+	        r: red.length === 2 ? red : "0"+red,
+	        g: grn.length === 2 ? grn : "0"+grn,
+	        b: blu.length === 2 ? blu : "0"+blu
+	    };
+	}
+
+	// Assign colors
+	for (var i = 0; i < $scope.results.length; i++)
+	{
+		col = HSVtoRGB(0+i*360/$scope.results.length,0.5,0.8);
+		$scope.results[i].color = '#' + col.r + col.g + col.b;
+	}
 
 })
