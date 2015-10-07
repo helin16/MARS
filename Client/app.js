470817381e079fd8161a4258b8ebf67d7b9c80ba
Nathan <nathansherburn@gmail.com>
2015-10-07 15:03:56 +1100

    Everything is broken

--- Client/app.js
+++ Client/app.js
@@ -1,60 +1,180 @@
-var app = angular.module('melts', ['ngMaterial', 'ui.router']);
+var app = angular.module('melts', ['ngMaterial', 'ui.router', 'btford.socket-io']);
 
-app.controller('MainController', function($scope, $mdSidenav) {
+app.controller('MainController', ['$scope', '$mdSidenav', function($scope, $mdSidenav) {
 
-})
+}])
 
 
 // TODO: Move these factories to the services folder
-.factory('questionResponseSocket', function ($rootScope) {
-  var socket = io.connect('/response');
-  return {
-    on: function (eventName, callback) {
-      socket.on(eventName, function () {  
-        var args = arguments;
-        $rootScope.$apply(function () {
-          // these $applys are necessary for js data binding
-          callback.apply(socket, args);
-        });
-      });
-    },
-    emit: function (eventName, data, callback) {
-      socket.emit(eventName, data, function () {
-        var args = arguments;
-        $rootScope.$apply(function () {
-          if (callback) {
-            callback.apply(socket, args);
-          }
-        });
-      })
-    }
-  };
-})
+.factory('responseSocket', ['socketFactory', function (socketFactory) {
+  var responseIoSocket = io.connect('/response');
 
-.factory('questionEditingSocket', function ($rootScope) {
-  var socket = io.connect('/editing');
-  return {
-    on: function (eventName, callback) {
-      socket.on(eventName, function () {  
-        var args = arguments;
-        $rootScope.$apply(function () {
-          // these $applys are necessary for js data binding
-          callback.apply(socket, args);
-        });
-      });
-    },
-    emit: function (eventName, data, callback) {
-      socket.emit(eventName, data, function () {
-        var args = arguments;
-        $rootScope.$apply(function () {
-          if (callback) {
-            callback.apply(socket, args);
-          }
-        });
-      })
+  var responseSocket = socketFactory({
+    ioSocket: responseIoSocket
+  });
+
+  return responseSocket;
+}])
+
+.factory('questionManager', ['$rootScope', '$timeout', 'responseSocket', function ($rootScope, $timeout, responseSocket) {
+
+  // get subscriptions for student
+
+  // make object/array for current active sessions
+  // - holds current question state (the question the user is attempting right now)
+  // - allows toggle of current question
+  // - holds data (answers and info) for each question
+
+  // set up socket listener for new sessions - feed to aformentioned obj/arr
+
+  // set up socket listener for disappearing sessions - keep aformentioned obj/arr updated
+
+  // check for currently active sessions
+
+  // get questions for active sessions - feed to aformentioned obj/arr
+
+  // listen for answer submissions from the user
+
+  // provides a way for answer controller to know if user has subscribed to something
+
+
+
+  // NOTE: Upon recieving new questions, 
+  // check if activeQuesitons length === 1 and if so, set it as selectedQuestion
+  // otherwise just add it to activeQuestions
+  var activeQuestions = [];
+  var pollIndex = 0;
+  var questionManager = {};
+
+  questionManager.subscribed = false;
+  questionManager.selectedQuestion = null;
+
+  // Allows controllers to subscribe to updates to do with activeQuestions array
+  questionManager.subscribeQuestionList = function(scope, callback) {
+    var handler = $rootScope.$on('activeQuestionsUpdate', callback);
+    scope.$on('$destroy', handler); // destroys the listener above
+  }
+
+  // Used to notify controllers that the question list has changed
+  function emitQuestionListChange () {
+    $rootScope.$emit('activeQuestionsUpdate');
+  }
+
+  questionManager.submitAnswer = function (pluginData) {
+    var answer = {
+      id: questionManager.selectedQuestion.id,
+      // userId will be filled in on server
+      data: pluginData
     }
-  };
-})
+    responseSocket.emit('new answer', answer)
+  }
+
+  // Upon new questions being opened by a lecturer
+  // responseSocket.on('new question', function(data){
+  function responseSocketOnNewQuestion (data) {// this is temp for testing - use line above in production
+    // push question data onto activeQuestions array
+    activeQuestions.push(data)
+
+    // if there is only 1 question in activeQuestions array, automatically make it the selectedQuestion
+    if (activeQuestions.length === 1) {
+      questionManager.selectedQuestion = activeQuestions[0];
+    }
+
+    emitQuestionListChange();
+  }
+  // );
+
+
+
+  // function subscribedChange () {
+  //   questionManager.subscribed = !questionManager.subscribed
+  //   console.log(questionManager.subscribed)
+  // }
+  // $timeout(subscribedChange, 1000);
+
+  // Simulate retrieving active polls
+  $timeout(activeQuestionsRecieved, 1000);
+
+  // Simulate recieving a new poll
+  $timeout(newPoll, 30000, true, 0);
+  $timeout(newPoll, 20000, true, 1);
+  $timeout(newPoll, 10000, true, 2);
+
+  function activeQuestionsRecieved() {
+    var data = {
+      id: "1928he98eh219e21e9",
+      pluginType: "multipleChoice",
+      collection: "ENG1001",
+      question: "What is this subject? An ideal inverting Op Amp?",
+      answers: [
+      { id: 0, label: 'The gain, G, would be 0.3333' },
+      { id: 1, label: 'The gain, G, would be 3' },
+      { id: 2, label: 'The gain, G, would be 3*10^4' },
+      { id: 3, label: 'The gain, G, would be -3' }
+      ]
+    } 
+    responseSocketOnNewQuestion(data);
+  }
+
+  function newPoll (i) {
+    var polls = [
+    {
+      id: "1928he98eh219e2112",
+      pluginType: "multipleChoice",
+      collection: "ENG1004",
+      question: "A new unit full of potential?",
+      answers: [
+      { id: 3, label: 'The gain, G, would be -3' },
+      { id: 2, label: 'The gain, G, would be 3*10^4' },
+      { id: 1, label: 'The gain, G, would be 3' },
+      { id: 0, label: 'The gain, G, would be 0.3333' },
+      ]
+    },
+    {
+      id: "1928he98eh219e2110",
+      pluginType: "multipleChoice",
+      collection: "ENG1002",
+      question: "Assuming an ideal inverting Op Amp, what would the gain be, if R1 was 300k ohms and R2 was 100k?",
+      answers: [
+      { id: 2, label: 'The gain, G, would be 3*10^4' },
+      { id: 0, label: 'The gain, G, would be 0.3333' },
+      { id: 3, label: 'The gain, G, would be -3' },
+      { id: 1, label: 'The gain, G, would be 3' },
+      ]
+    },
+    {
+      id: "1928he98eh219e2111",
+      pluginType: "multipleChoice",
+      collection: "ENG1003",
+      question: "How many different data types are there in JavaScript?",
+      answers: [
+      { id: 0, label: 'The gain, G, would be 0.3333' },
+      { id: 3, label: 'The gain, G, would be -3' },
+      { id: 1, label: 'The gain, G, would be 3' },
+      { id: 2, label: 'The gain, G, would be 3*10^4' },
+      ]
+    }
+    ]
+    responseSocketOnNewQuestion(polls[i]);
+  }
+
+  // function nextQuestion () {
+  //   return 'answer.plugin_' + $scope.selectedQuestion.pluginType  
+  // }
+
+  return questionManager;
+}])
+
+.factory('editingSocket', ['socketFactory', function (socketFactory) {
+  var editingIoSocket = io.connect('/editing');
+
+  var editingSocket = socketFactory({
+    ioSocket: editingIoSocket
+  });
+
+  return editingSocket;
+}])
+
 
 .config(function($stateProvider, $urlRouterProvider, $mdThemingProvider) {
 
@@ -126,7 +246,7 @@ app.controller('MainController', function($scope, $mdSidenav) {
   });
 
   // TODO retrieve available plugins via AJAX
-  var plugins = ['poll', 'draw', 'wordcloud'];	// This is a temporary array to fake this data
+  var plugins = ['multipleChoice', 'draw', 'wordCloud'];	// This is a temporary array to fake this data
 
   // Generates a plugin's controller name based on it's plugin name
   controllerName = function(pluginName) {
--- Client/student/answer.js
+++ Client/student/answer.js
@@ -3,149 +3,33 @@ app.controller('AnswerController', function(
   $mdDialog,
   $timeout,
   $state,
-  questionResponseSocket
+  questionManager
   ) {
 
-  // TODO Add service to notify this controller of new questions
-  // NOTE: Upon recieving new questions, 
-  // check if activeQuesitons length === 1 and if so, set it as selectedPoll
-  // otherwise just add it to activePolls
-
-  // TODO Add service to notify this controller of questions ending
-
-
-
-
-  // socketio demo
-  $scope.socketMe = function() {
-    console.log('answer')
-    questionResponseSocket.emit('answer', {
-      questionId: "09328n0nd9f0jdfxcvf",
-      responseData: "{blah:'blah',hey:'hey'}"})
-  }
+  // // TODO Add service to notify this controller of new questions / questions ending
+  questionManager.subscribeQuestionList($scope, function() { 
+    // Set the app state to answer.plugin_<pluginType>
+    $state.go('answer.plugin_' + questionManager.selectedQuestion.pluginType)
+  })
 
   $scope.logout = function(){
     $state.go('logout');
   }
 
-  // Front page (holds all active polls from subscribed collections)
-  $scope.subscribed = true;
-  $scope.activePolls = [];
-  $scope.selectedPoll = null;
-  $scope.pollIndex = 0;
-
-  // Simulate retrieving active polls
-  $timeout(activePollsRecieved, 1000);
-
-  // Simulate recieving a new poll
-  $timeout(newPoll, 30000, true, 0);
-  $timeout(newPoll, 20000, true, 1);
-  $timeout(newPoll, 10000, true, 2);
-
-  if ($scope.subscribed) {
+  if (questionManager.subscribed) {
     $state.go('answer.subscribed');
   } else {
     $state.go('answer.notsubscribed');
   }
 
-  function activePollsRecieved() {
-    $scope.activePolls = [
-    {
-      id: "1928he98eh219e21e9",
-      pluginType: "poll",
-      collection: "ENG1001",
-      question: "What is this subject? An ideal inverting Op Amp?",
-      answers: [
-      { id: 0, label: 'The gain, G, would be 0.3333' },
-      { id: 1, label: 'The gain, G, would be 3' },
-      { id: 2, label: 'The gain, G, would be 3*10^4' },
-      { id: 3, label: 'The gain, G, would be -3' }
-      ]
-    },
-    ];
-
-  // Set first poll as active poll
-  if ($scope.activePolls.length > 0) {
-    $scope.selectedPoll = $scope.activePolls[0];
-    // Set the app state to answer.<pluginType>
-    $state.go('answer.plugin_' + $scope.selectedPoll.pluginType)
-    }
-  }
-
-  function newPoll (i) {
-    var polls = [
-    {
-      id: "1928he98eh219e2112",
-      pluginType: "poll",
-      collection: "ENG1004",
-      question: "A new unit full of potential?",
-      answers: [
-      { id: 3, label: 'The gain, G, would be -3' },
-      { id: 2, label: 'The gain, G, would be 3*10^4' },
-      { id: 1, label: 'The gain, G, would be 3' },
-      { id: 0, label: 'The gain, G, would be 0.3333' },
-      ]
-    },
-    {
-      id: "1928he98eh219e2110",
-      pluginType: "poll",
-      collection: "ENG1002",
-      question: "Assuming an ideal inverting Op Amp, what would the gain be, if R1 was 300k ohms and R2 was 100k?",
-      answers: [
-      { id: 2, label: 'The gain, G, would be 3*10^4' },
-      { id: 0, label: 'The gain, G, would be 0.3333' },
-      { id: 3, label: 'The gain, G, would be -3' },
-      { id: 1, label: 'The gain, G, would be 3' },
-      ]
-    },
-    {
-      id: "1928he98eh219e2111",
-      pluginType: "poll",
-      collection: "ENG1003",
-      question: "How many different data types are there in JavaScript?",
-      answers: [
-      { id: 0, label: 'The gain, G, would be 0.3333' },
-      { id: 3, label: 'The gain, G, would be -3' },
-      { id: 1, label: 'The gain, G, would be 3' },
-      { id: 2, label: 'The gain, G, would be 3*10^4' },
-      ]
-    }
-    ]
-
-    $scope.activePolls.push(polls[i])
-    console.log($scope.activePolls)
-    if ($scope.activePolls.length === 1) {
-      $scope.selectedPoll = $scope.activePolls[0];
-    }
-  }
-
-  // $scope.selectPoll = function (poll) {
-  // 	console.log(poll)
-  // 	$scope.selectedPoll = poll;
-  // }
-
   $scope.nextPoll = function (event) {
-    if ($scope.pollIndex > $scope.activePolls.length - 2) {
-      $scope.pollIndex = 0;
-    } else {
-      $scope.pollIndex++;
-    }
-    $scope.selectedPoll = $scope.activePolls[$scope.pollIndex]
-    $state.go('answer.plugin_' + $scope.selectedPoll.pluginType)
+    // iterate through questions on questionManager which will return state/view
+    // var questionType
+    $state.go(questionType)
   }
 
-  // $scope.submit = function () {
-  // 	console.log("Sending answers...")
-  // 	$scope.selectedPoll = null;
-  // }
-
-  $scope.submitAnswer = function () {
-    console.log("sending answer");
-  // send poll id and answer through socket io
-  }
-
-  $scope.alert = '';
-  $scope.showAlert = function(ev) {
+  $scope.joinMessage = '';
+  $scope.showJoinDialog = function(ev) {
     $mdDialog.show({
       controller: 'JoinFeedController',
       templateUrl: 'student/answer.join.html',
@@ -154,11 +38,11 @@ app.controller('AnswerController', function(
     })
     .then(
       function(answer) {
-        $scope.alert = 'You said the information was "' + answer + '".';
+        $scope.joinMessage = 'You said the information was "' + answer + '".';
       },
       function() {
-        $scope.alert = 'You cancelled the dialog.';
+        $scope.joinMessage = 'You cancelled the dialog.';
       }
-      );
+    );
   };
 })
