6e8b5e343a3f3f6327ad72dd8c276d7a1d96305c
Nathan <nathansherburn@gmail.com>
2015-10-07 15:03:56 +1100

    Everything is broken

--- Client/plugins/poll/answer/answer.poll.js
+++
@@ -1,9 +0,0 @@
-app.controller('PollPluginController', function($scope) {
-
-	// TODO Request $routeParams.pollId poll;
-
-	// TODO Add socket io service for posting live answers
-
-	// TODO Add socket io service for re routing to the poll feed
-
-})
